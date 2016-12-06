2f93d7277fb209e37f52e381569f2d044597c5fa
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2016-12-07 02:42:07 +0900

    overwrite

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,12 @@ var express = require('express');
 var router = express.Router();
 var cookie = require('cookie-parser');
 var mongojs = require('mongojs');
-var db = mongojs('memo', ['users']);
-
-// db.test.find({}, {name:1}, function(error, data) {
-// 	console.log(error);
-// 	console.log(data);
-// });
+var db = mongojs('memodb', ['users', 'memos']);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+	var data = {};
 
 	//쿠키 있으면 조회
 	if(req.cookies.sessionID != undefined) {
@@ -19,33 +16,40 @@ router.get('/', function(req, res, next) {
 		// console.log(req.sessionID);
 
 		//DB에서 세션아이디로 메모 조회
+		db.memos.find({sessionId: req.cookies.sessionID}, function(err, docs) {
+			console.log(docs);
+			data['memos'] = docs;
 
+			res.render('index', data);
+		});
 	//쿠키 없으면 세션ID를 DB에 저장하고 쿠키 저장
 	}else {
 		// console.log('쿠키 없음!!');
 		// console.log(req.cookies.sessionID);
 		// console.log(req.sessionID);
 
+		db.users.insert({sessionId: req.sessionID});
 		res.cookie('sessionID', req.sessionID);
+
+		res.render('index', data);
 	}
 
-	var data = {};
-	var memos = [];
-	var memo1 = {"memoId":1, "title":"타이틀입니다."};
-	var memo2 = {"memoId":2, "title":"타이틀입니다.222"};
+	
+	// var memos = [];
+	// var memo1 = {"memoId":1, "title":"타이틀입니다."};
+	// var memo2 = {"memoId":2, "title":"타이틀입니다.222"};
 
-	memos.push(memo1);
-	memos.push(memo2);
-	data['memos'] = memos;
-
-	res.render('index', data);
+	// memos.push(memo1);
+	// memos.push(memo2);
+	
 });
 
 router.get('/destroy', function(req, res, next) {
 	var sessionId = req.cookies.sessionID;
 
 	//DB 삭제
-	
+	db.memos.find({sessionId: sessionId}).remove();
+	db.users.find({sessionId: sessionId}).remove();
 
 	res.clearCookie('sessionID');
 	res.redirect('/');
@@ -61,25 +65,40 @@ router.get('/memo', function(req, res, next) {
 
 router.get('/memo/:memoId', function(req, res, next) {
 	var sessionId = req.cookies.sessionID;
+	var _id = req.params.memoId;
 	var data = {};
-	data['newFlag'] = false;
-	data['title'] = "타이틀입니다.";
-	data['content'] = "내용입니다.";
-
-	res.render('memo', data);
+	
+	db.memos.findOne({
+	    _id: _id/*mongojs.ObjectId('523209c4561c640000000001')*/
+	}, function(err, doc) {
+		console.log('aaaaa')
+		console.log(doc);
+		data['newFlag'] = false;
+		data['memoId'] = doc._id;
+		data['title'] = doc.title;
+		data['content'] = doc.content;    
+	    res.render('memo', data);
+	})	
 });
 
 router.post('/create', function(req, res, next) {
 	var sessionId = req.cookies.sessionID;
+	var title = req.body.title;
+	var content = req.body.content;
 	//메모 저장
 
+	db.memos.insert({sessionId: sessionId, title: title, content: content});
 	res.redirect('/');
 });
 
 router.post('/save', function(req, res, next) {
 	var sessionId = req.cookies.sessionID;
+	var _id = req.body.memoId;
+	var title = req.body.title;
+	var content = req.body.content;
 	//메모 수정
 
+	db.memos.find({_id: _id}).update({$title: title, $content: content})
 	res.redirect('/');
 });
 
@@ -87,6 +106,7 @@ router.post('/delete', function(req, res, next) {
 	var sessionId = req.cookies.sessionID;
 	//메모 삭제
 
+	db.memos.find({_id: _id}).removeOne();
 	res.redirect('/');
 });
 
